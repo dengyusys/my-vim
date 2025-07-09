84ad6a4cbd506c8c93e0e24bbfbb8fbbed2d0444
dengyusys <dy447388875@163.com>
2025-07-09 17:31:27 +0800

    <feat>wezterm配置

---
+++ .wezterm.lua
@@ -0,0 +1,19 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = 'AdventureTime'
+
+-- 设置使用shell
+config.default_prog = { 'C:/Program Files/PowerShell/7/pwsh.exe' }
+
+-- 取消连字
+onfig.harfbuzz_features = { 'calt = 0', 'clig = 0', 'liga = 0' }
+
+-- and finally, return the configuration to wezterm
+return config
--- install.ps1
+++ install.ps1
@@ -13,3 +13,19 @@ if (Test-Path -Path $destinationFile) {
 } else {
     Write-Host "复制失败，请检查权限或文件路径"
 }
+
+# 获取当前目录下的.wezterm.lua文件
+$sourceFileWezterm = Join-Path -Path $PWD -ChildPath ".wezterm.lua"
+
+# 设置目标文件路径（用户主目录下的.wezterm.lua）
+$destinationFileWezterm = Join-Path -Path $HOME -ChildPath ".wezterm.lua"
+
+# 复制文件，如果目标文件已存在则覆盖
+Copy-Item -Path $sourceFileWezterm -Destination $destinationFileWezterm -Force
+
+# 输出操作结果
+if (Test-Path -Path $destinationFileWezterm) {
+    Write-Host ".wezterm.lua文件已成功复制到 $destinationFileWezterm"
+} else {
+    Write-Host "复制失败，请检查权限或文件路径"
+}
